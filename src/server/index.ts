abba35874e58a2ae4035301a974f09c549fb5589
r253.hmdryou <r253.hmdryou@gmail.com>
2022-07-18 09:39:55 +0900

    add index.ts

---
+++ src/server/index.ts
@@ -0,0 +1,28 @@
+import express from "express";
+
+main();
+
+/**
+ * main function
+ * @returns void
+ */
+function main(): void {
+	const app = createApp();
+
+	app.listen(3000);
+}
+
+/**
+ *
+ * @returns express application
+ */
+function createApp(): express.Express {
+	return express()
+		.set("json spaces", 2)
+		.use(express.json({strict: true}))
+		.use(express.urlencoded({extended: true}))
+
+		.get("/", (_req, res) => {
+			res.send("Hello World!");
+		});
+}
