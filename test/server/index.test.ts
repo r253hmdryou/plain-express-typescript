17a352c293dd1179d7105ae38306bd9890d945a9
r253.hmdryou <r253.hmdryou@gmail.com>
2022-08-20 00:22:07 +0900

    add error handling test

--- test/server/index.test.ts
+++ test/server/index.test.ts
@@ -1,5 +1,7 @@
+import express from "express";
 import request from "supertest";
 import app from "app";
+import { errorHandler } from "routes";
 
 {
 	describe("error handling", testError);
@@ -33,6 +35,20 @@ function testError() {
 				code: "internalServerError",
 				message: "Internal Server Error",
 			});
+		}
+
+		// error handling
+		{
+			const res = {
+				status: jest.fn().mockReturnThis(),
+				json: jest.fn().mockReturnThis(),
+			};
+			errorHandler(new Error("test"), {} as express.Request, res as unknown as express.Response, {} as express.NextFunction);
+			expect(res.status.mock.calls).toEqual([[500]]);
+			expect(res.json.mock.calls).toEqual([[{
+				code: "internalServerError",
+				message: "Internal Server Error",
+			}]]);
 		}
 	});
 }
